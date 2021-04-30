fdeec5873cc54e50d8f3316efe253b6d58a61d64
xxxsssyyy <xsy961127@buaa.edu.cn>
2021-04-30 10:34:20 +0800

    fix some bug with transformer

--- tests/test_Transformer_CRF.py
+++ tests/test_Transformer_CRF.py
@@ -8,14 +8,15 @@ from collections import OrderedDict
 sys.path.append('../')
 
 import tensorflow as tf
+from models.Transformer_CRF import TransformerCRFModel
 import numpy as np
-from models.Transformer_CRF import Model
 from utils.loader import load_sentences, update_tag_scheme
 from utils.loader import char_mapping, tag_mapping
 from utils.loader import augment_with_pretrained, prepare_padding_dataset
 from utils.utils import get_logger, make_path, clean, create_model, save_model
 from utils.utils import print_config, save_config, load_config, test_ner
 from utils.data_utils import load_word2vec, create_input, input_from_line_padding, BatchManager
+from utils.metric import get_ner_fmeasure, recover_label
 
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "7"
@@ -25,22 +26,18 @@ flags = tf.app.flags
 flags.DEFINE_boolean("clean",       False,      "clean train folder")
 flags.DEFINE_boolean("train",       False,      "Wither train the model")
 # configurations for the model
-flags.DEFINE_integer("seg_dim",     20,         "Embedding size for segmentation, 0 if not used")
-flags.DEFINE_integer("char_dim",    100,        "Embedding size for characters")
-flags.DEFINE_integer("lstm_dim",    100,        "Num of hidden units in LSTM")
-flags.DEFINE_string("tag_schema",   "iobes",      "tagging schema iobes or iob")
-flags.DEFINE_integer("layers",      1,          "Num of hidden layers")
+flags.DEFINE_integer("num_units",    100,        "Embedding size for characters")
+flags.DEFINE_string("tag_schema",   "iob",      "tagging schema iobes or iob")
 flags.DEFINE_integer("heads",       4,          "Num of multi-attention heads")
 flags.DEFINE_integer("max_seq_len", 100,        "Num of max len of sequence")
-flags.DEFINE_integer("num_blocks",  1,          "Num blocks")
+flags.DEFINE_integer("num_blocks",  2,          "Num blocks")
 
 # configurations for training
 flags.DEFINE_float("clip",          5,          "Gradient clip")
-flags.DEFINE_float("dropout",       0.5,        "Dropout rate")
+flags.DEFINE_float("dropout",       0.1,        "Dropout rate")
 flags.DEFINE_float("batch_size",    20,         "batch size")
 flags.DEFINE_float("lr",            0.001,      "Initial learning rate")
 flags.DEFINE_string("optimizer",    "adam",     "Optimizer for training") 
-flags.DEFINE_boolean("pre_emb",     False,       "Wither use pre-trained embedding")
 flags.DEFINE_boolean("zeros",       False,      "Wither replace digits with zero")
 flags.DEFINE_boolean("lower",       True,       "Wither lower case")
 
@@ -56,9 +53,9 @@ flags.DEFINE_string("config_file",  flags.FLAGS.save_path+"/config_file",  "File
 flags.DEFINE_string("script",       "conlleval",    "evaluation script")
 flags.DEFINE_string("result_path",  flags.FLAGS.save_path+"/result",       "Path for results")
 flags.DEFINE_string("emb_file",     "../data/wiki_100.utf8", "Path for pre_trained embedding")
-flags.DEFINE_string("train_file", "../data/chinese_data2/train.char.bmes",  "Path for train data")
-flags.DEFINE_string("dev_file", "../data/chinese_data2/dev.char.bmes",    "Path for dev data")
-flags.DEFINE_string("test_file", "../data/chinese_data2/test.char.bmes",   "Path for test data")
+flags.DEFINE_string("train_file", "../data/example.train",  "Path for train data")
+flags.DEFINE_string("dev_file", "../data/example.dev",    "Path for dev data")
+flags.DEFINE_string("test_file", "../data/example.test",   "Path for test data")
 
 
 FLAGS = tf.app.flags.FLAGS
@@ -72,48 +69,23 @@ assert FLAGS.optimizer in ["adam", "sgd", "adagrad"]
 def config_model(char_to_id, tag_to_id):
     config = OrderedDict()
     config["num_chars"] = len(char_to_id)
-    config["char_dim"] = FLAGS.char_dim
+    config["num_units"] = FLAGS.num_units
     config["num_tags"] = len(tag_to_id)
-    config["seg_dim"] = FLAGS.seg_dim
-    config["lstm_dim"] = FLAGS.lstm_dim
+
     config["batch_size"] = FLAGS.batch_size
-    config["layers"] = FLAGS.layers
     config["heads"] = FLAGS.heads
     config["max_seq_len"] = FLAGS.max_seq_len
     config["num_blocks"] = FLAGS.num_blocks
 
     config["emb_file"] = FLAGS.emb_file
     config["clip"] = FLAGS.clip
-    config["dropout_keep"] = 1.0 - FLAGS.dropout
+    config["dropout"] = 1.0 - FLAGS.dropout
     config["optimizer"] = FLAGS.optimizer
     config["lr"] = FLAGS.lr
     config["tag_schema"] = FLAGS.tag_schema
-    config["pre_emb"] = FLAGS.pre_emb
     config["zeros"] = FLAGS.zeros
     config["lower"] = FLAGS.lower
     return config
-
-
-def evaluate(sess, model, name, data, id_to_tag, logger):
-    logger.info("evaluate:{}".format(name))
-    ner_results = model.evaluate(sess, data, id_to_tag)
-    eval_lines = test_ner(ner_results, FLAGS.result_path)
-    for line in eval_lines:
-        logger.info(line)
-    f1 = float(eval_lines[1].strip().split()[-1])
-
-    if name == "dev":
-        best_test_f1 = model.best_dev_f1.eval()
-        if f1 > best_test_f1:
-            tf.assign(model.best_dev_f1, f1).eval()
-            logger.info("new best dev f1 score:{:>.3f}".format(f1))
-        return f1 > best_test_f1, f1
-    elif name == "test":
-        best_test_f1 = model.best_test_f1.eval()
-        if f1 > best_test_f1:
-            tf.assign(model.best_test_f1, f1).eval()
-            logger.info("new best test f1 score:{:>.3f}".format(f1))
-        return f1 > best_test_f1, f1
 
 
 def train():
@@ -123,23 +95,13 @@ def train():
     test_sentences = load_sentences(FLAGS.test_file, FLAGS.lower, FLAGS.zeros)
 
     # Use selected tagging scheme (IOB / IOBES)
-    update_tag_scheme(train_sentences, FLAGS.tag_schema)
-    update_tag_scheme(test_sentences, FLAGS.tag_schema)
+    #update_tag_scheme(train_sentences, FLAGS.tag_schema)
+    #update_tag_scheme(test_sentences, FLAGS.tag_schema)
 
     # create maps if not exist
     if not os.path.isfile(FLAGS.map_file):
         # create dictionary for word
-        if FLAGS.pre_emb:
-            dico_chars_train = char_mapping(train_sentences, FLAGS.lower)[0]
-            dico_chars, char_to_id, id_to_char = augment_with_pretrained(
-                dico_chars_train.copy(),
-                FLAGS.emb_file,
-                list(itertools.chain.from_iterable(
-                    [[w[0] for w in s] for s in test_sentences])
-                )
-            )
-        else:
-            _c, char_to_id, id_to_char = char_mapping(train_sentences, FLAGS.lower)
+        _c, char_to_id, id_to_char = char_mapping(train_sentences, FLAGS.lower)
 
         # Create a dictionary and a mapping for tags
         _t, tag_to_id, id_to_tag = tag_mapping(train_sentences)
@@ -160,12 +122,24 @@ def train():
     test_data = prepare_padding_dataset(
         test_sentences, FLAGS.max_seq_len, char_to_id, tag_to_id, FLAGS.lower
     )
+    
+    
     print("%i / %i / %i sentences in train / dev / test." % (
-        len(train_data), 0, len(test_data)))
+        len(train_data), len(dev_data), len(test_data)))
 
     train_manager = BatchManager(train_data, FLAGS.batch_size)
     dev_manager = BatchManager(dev_data, 100)
     test_manager = BatchManager(test_data, 100)
+
+    """
+    batch = train_manager.batch_data[0]
+    strings, chars, segs, tags = batch
+    for chrs in chars:
+        print(chrs)
+    for chrs in segs:
+        print(chrs)
+    print(tag_to_id)
+    """
     # make path for store log and model if not exist
     make_path(FLAGS)
     if os.path.isfile(FLAGS.config_file):
@@ -174,7 +148,7 @@ def train():
         config = config_model(char_to_id, tag_to_id)
         save_config(config, FLAGS.config_file)
     make_path(FLAGS)
-
+    
     log_path = os.path.join(FLAGS.save_path, "log", FLAGS.log_file)
     logger = get_logger(log_path)
     print_config(config, logger)
@@ -184,7 +158,8 @@ def train():
     tf_config.gpu_options.allow_growth = True
     steps_per_epoch = train_manager.len_data
     with tf.Session(config=tf_config) as sess:
-        model = create_model(sess, Model, FLAGS.ckpt_path, load_word2vec, config, id_to_char, logger)
+        model = TransformerCRFModel(config, is_training=True)
+        sess.run(tf.global_variables_initializer())
         logger.info("start training")
         loss = []
         for i in range(100):
@@ -198,31 +173,55 @@ def train():
                         iteration, step%steps_per_epoch, steps_per_epoch, np.mean(loss)))
                     loss = []
 
-            best, dev_f1_score = evaluate(sess, model, "dev", dev_manager, id_to_tag, logger)
-            #if best:
-            save_model(sess, model, FLAGS.ckpt_path, logger)
-            best, test_f1_score = evaluate(sess, model, "test", test_manager, id_to_tag, logger)
-            logger.info("Epoch:{}, dev f1-score:{:>5.3f}, test f1-score: {:>5.3f}".format(i+1, dev_f1_score, test_f1_score))
+            predict_lists = []
+            source_tag = []
+            best_dev_f1 = 0.0
+            best_test_f1 = 0.0
+            for batch in dev_manager.iter_batch(shuffle=False):
+                lengths, logits = model.run_step(sess, False, batch)
+                _, chars, segs, tags = batch
+                transition = model.transition.eval(session=sess)
+                pre_seq = model.predict(logits, transition, lengths)
+                pre_label = recover_label(pre_seq, lengths, id_to_tag)
+                """
+                for p in range(len(pre_label)):
+                    print(chars[p])
+                    print(pre_label[p])
+                """
+                source_label = recover_label(tags, lengths, id_to_tag)
+                predict_lists.extend(pre_label)
+                source_tag.extend(source_label)
+            train_loss_v = np.round(float(np.mean(loss)), 4)
+            print('****************************************************')
+            acc, p, r, f = get_ner_fmeasure(source_tag, predict_lists, config["tag_schema"])
+            logger.info('epoch:\t{}\ttrain loss:\t{}\t'.format(i+1, train_loss_v))
+            logger.info('dev acc:\t{}\tp:\t{}\tr:\t{}\tf:\t{}'.format(acc, p, r, f))
+            
+            
+            for batch in test_manager.iter_batch(shuffle=False):
+                lengths, logits = model.run_step(sess, False, batch)
+                _, chars, segs, tags = batch
+                transition = model.transition.eval(session=sess)
+                pre_seq = model.predict(logits, transition, lengths)
+                pre_label = recover_label(pre_seq, lengths, id_to_tag)
+                source_label = recover_label(tags, lengths, id_to_tag)
+                predict_lists.extend(pre_label)
+                source_tag.extend(source_label)
 
+            acc_t, p_t, r_t, f_t = get_ner_fmeasure(source_tag, predict_lists, config["tag_schema"])
+            logger.info('test acc:\t{}\tp:\t{}\tr:\t{}\tf:\t{}'.format(acc_t, p_t, r_t, f_t))
+            if f > best_dev_f1:
+                save_model(sess, model, FLAGS.ckpt_path, logger)
+                best_dev_f1 = f
+                best_test_f1 = f_t
+                logger.info('save epoch:\t{} model with best dev f1-score'.format(i+1))
+            
+            print('****************************************************\n\n')
 
-def evaluate_line():
-    config = load_config(FLAGS.config_file)
-    logger = get_logger(FLAGS.log_file)
-    # limit GPU memory
-    tf_config = tf.ConfigProto()
-    tf_config.gpu_options.allow_growth = True
-    with open(FLAGS.map_file, "rb") as f:
-        char_to_id, id_to_char, tag_to_id, id_to_tag = pickle.load(f)
-    with tf.Session(config=tf_config) as sess:
-        model = create_model(sess, Model, FLAGS.ckpt_path, load_word2vec, config, id_to_char, logger)
-        while True:
-                line = input("Please input your query:")
-                result = model.evaluate_line(sess, input_from_line_padding(line, char_to_id, FLAGS.max_seq_len), id_to_tag)
-                print(result)
+            
 
 
 def main(_):
-    
     FLAGS.train = True
     FLAGS.clean = True
     clean(FLAGS)
@@ -233,4 +232,3 @@ def main(_):
 
 if __name__ == "__main__":
     tf.app.run(main)
-
